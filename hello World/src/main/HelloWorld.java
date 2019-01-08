cd9b15d8fa4aa0c7d904b089718e369d86861a17
Younghwan Jeon <jeonyounghwan86@gmail.com>
2019-01-08 12:12:17 +0900

    1

--- hello World/src/main/HelloWorld.java
+++ hello World/src/main/HelloWorld.java
@@ -4,12 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("World's The End~~~~~~~~~");
+		System.out.println("World's The End!!!!!!!!!dassda~~");
 		System.out.println("TEST The End");
-		System.out.println("World's Hell");
-		System.out.println("World's Hell");
-		System.out.println("World's Hell");
-		System.out.println("World's Hell");
 		System.out.println("World's Hell");
 	}
 
