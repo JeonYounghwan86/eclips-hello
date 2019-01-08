67004194809741fdd5de2733bf69be9c58a4a15e
Younghwan Jeon <jeonyounghwan86@gmail.com>
2019-01-08 12:19:37 +0900

    666666

--- hello World/src/main/HelloWorld.java
+++ hello World/src/main/HelloWorld.java
@@ -3,7 +3,13 @@ package main;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("World");
+		System.out.println("TestWorld");
+		System.out.println("WorldWeb");
+		System.out.println("World1");
+		System.out.println("World2");
+		System.out.println("World3");
+		System.out.println("World4");
+		System.out.println("World5");
 	}
 
 }
